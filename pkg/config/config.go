0f981ef62ec1026788a52ee647206abe46e19be7
bogi <bogdan.ristic@codebehind.rs>
2023-12-07 21:06:04 +0100

    add config pkg

---
+++ pkg/config/config.go
@@ -0,0 +1,28 @@
+package config
+
+import (
+	"fmt"
+	"os"
+)
+
+type Config struct {
+	ListenAddr string
+	DBUri      string
+}
+
+func ReadConfig() (*Config, error) {
+	listenAddr, ok := os.LookupEnv("LISTEN_ADDR")
+	if !ok {
+		return nil, fmt.Errorf("LISTEN_ADDR is required but was not set")
+	}
+
+	dbUri, ok := os.LookupEnv("DB_URI")
+	if !ok {
+		return nil, fmt.Errorf("DB_URI is required but was not set")
+	}
+
+	return &Config{
+		ListenAddr: listenAddr,
+		DBUri:      dbUri,
+	}, nil
+}
